11032ce8325a4d93a8de783a0a38d7d314fa12c2
Recep H <sample@gmail.com>
2022-06-09 10:44:02 +0300

    Angular material added.

--- projects/my-first-app/src/app/app.component.ts
+++ projects/my-first-app/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +12,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
 
   hiAction(){
     alert('Hi!')
--- projects/my-first-app/src/app/app.module.ts
+++ projects/my-first-app/src/app/app.module.ts
@@ -5,6 +5,11 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MyLibModule} from "my-lib";
 import {ProjectTypes} from "my-lib";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -20,7 +25,12 @@ import {ProjectTypes} from "my-lib";
         id: "123456",
         name: "John DOE"
       }
-    })
+    }),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
